0324d2a55ffd03393596b3713663090d77d4e06b
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2022-04-15 15:30:41 -0400

    Cleared repository ahead of thorough remodelling

--- README.md
+++
@@ -1,4 +0,0 @@
-<img src = 'https://github.com/farzanmirshekari/Waffle/blob/main/public/images/others/waffle-logo.svg' height = 150>
-<h1>Waffle</h1>
-
-Waffle is a social media platform, allowing users to specify an age range for their messages. An age-appropriate Reddit, anyone?
--- modules/validators.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-exports.signupValidator = [
-  body('username').exists().isString().isLength({ min: 4 }).bail(),
-  body('password').exists().isString().isLength({ min: 4 }).bail(),
-  body('confirmPassword').exists().isString()
-    .custom((value, { req }) => value === req.body.password)
-];
