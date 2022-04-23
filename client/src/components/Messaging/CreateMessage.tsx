6ebe4de42acf7c4c76e4b25f7ae664fb0bf0a8fa
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2022-04-23 11:55:15 -0400

    Create Message view underway

--- client/src/components/Messaging/CreateMessage.tsx
+++ client/src/components/Messaging/CreateMessage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import authentication_verifier from "../validators/authentication_verifier";
+import AbstractArtwork from '../../assets/abstract_still_life_artwork.jpg';
 
 function CreateMessage() {
 
@@ -20,8 +21,15 @@ function CreateMessage() {
 
     return (
 
-        <div className = 'full_page absolute w-full h-full flex flex-col justify-start items-center'>
-
+        <div className = 'full_page absolute w-full h-full flex flex-col justify-center items-center'>
+            <div className = 'message_creation_container relative w-4/6 h-4/6 flex flex-row justify-center items-center -translate-y-12'>
+                <div className = 'relative w-1/2 h-full justify-center items-center flex flex-col'>
+                    
+                </div>
+                <div className = 'relative w-1/2 h-full flex justify-center items-center'>
+                    <img src = {AbstractArtwork} className = 'relative w-full h-full' style = {{ borderTopRightRadius: `${10}px`, borderBottomRightRadius: `${10}px` }} alt = 'Login Artwork'/>
+                </div>
+            </div>
         </div>
 
     )
